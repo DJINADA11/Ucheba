05339cee424cf95441b73d5dd13c95ffee0eb656
DJINADA11 <95276159+DJINADA11@users.noreply.github.com>
2021-12-02 12:49:28 +0300

    Переделал код

--- Ignatev.py
+++ Ignatev.py
@@ -1,13 +1,37 @@
+print("Выбор функции")
+def vbor(t):
+
+    def tnow():
+                    '''Текущее время'''
+                    import datetime
+                    dt_now = datetime.datetime.now()
+                    return 1
+
+    def pramoug():
+                    print("Площадь прямоугольника ")
+                    from math import tan, pi
+                    s = int(input())
+                    n = int(input())
+                    g = (n * s ** 2) / (4 + tan(pi/n))
+                    print(g)
+                    return g
+
+    def summ():
+                    print("Сумма n чисел ")
+                    n = int(input())
+                    s = (n * (n + 1)) / 2
+                    print(s)
+                    return s
 
 
-def tnow():
-    '''Текущее время'''
-    import datetime
-    dt_now = datetime.datetime.now()
-    return dt_now
+    if t == 1:
+        i = pramoug()
+        return i
+    if t == 2:
+        i = summ()
+        return i
 
-def text():
-    word = 'Была вызвана функция: '
-    return word
 
-print (text(), tnow.__doc__, tnow())
+
+cmd = int(input())
+vbor(cmd)
